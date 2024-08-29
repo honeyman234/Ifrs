fa79643c23cf4e22760e7c53c94a86d837b4bc20
honeyman234 <104739897+honeyman234@users.noreply.github.com>
2024-08-29 12:11:58 +0100

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,167 @@
+/*const questions = [
+    {
+        question: "What is the purpose of IAS 1?",
+        answers: {
+            A: "To ensure consistent presentation of financial statements",
+            B: "To define the treatment of inventory",
+            C: "To guide on the recognition of revenue",
+            D: "To set out the principles for financial instruments"
+        },
+        correctAnswer: "A",
+        explanation: "IAS 1 focuses on the presentation of financial statements to ensure comparability."
+    },
+    // Add more questions here
+    {
+        question: "When was the International Accounting Standards Committee (IASC) established",
+        answers: {
+            A: "1886",
+            B: "1976",
+            C: "1973",
+            D: "2002"
+        },
+        correctAnswer: "C",
+        explanation: "The International Accounting Standards Committee (IASC) was established in 1973 "
+    },
+
+    {
+        question: "The first International Accounting Standards (IASs) were issued in _________",
+        answers: {
+            A: "1977",
+            B: "1975",
+            C: "1966",
+            D: "2001"
+        },
+        correctAnswer: "B",
+        explanation: "The first International Accounting Standards were issued in 1975."
+    },
+
+    {
+        question: "in 2001, the IASC and SIC were replaced by ______",
+        answers: {
+            A: "IFRSB and SEC",
+            B: "IASC and NASB",
+            C: "IASB and IFRIC",
+            D: "GAAP and FRSB"
+        },
+        correctAnswer: "C",
+        explanation: "The  IASC and SIC were replaced by IASB and IFRIC in 2001."
+    },
+
+    {
+        question: "What do we mean by Convergence of accounting standards",
+        answers: {
+            A: "the goal of establishing a single set of accounting standards that will be used internationally",
+            B: "the process of collapsing long and lengthy accounting standards into more managable ones",
+            C: "The transfer from GAAP and other accounting standards into IFRSs",
+            D: "The process of eliminating redudant accounting standards"
+        },
+        correctAnswer: "A",
+        explanation: "Convergence of accounting standards refers to the goal of establishing a single set of accounting standards that will be used internationally. This is also described as the international harmonisation of accounting standards."
+    },
+
+    {
+        question: "IFRS stands for ____________",
+        answers: {
+            A: "Integrated financial reporting standard",
+            B: "international financial reporting statements",
+            C: "International financial representation standard",
+            D: "International financial reporting standard"
+        },
+        correctAnswer: "D",
+        explanation: "IFRS stands for International Financial Reporting Standard."
+    },
+
+    {
+        question: "What does IAS stand for ",
+        answers: {
+            A: "International Accounting Standard",
+            B: "Integrated Accounting Standard",
+            C: "Interpreted Accounting Standards",
+            D: "International Accounting Statements"
+        },
+        correctAnswer: "A",
+        explanation: "IAS stands for International Accounting Standard."
+    },
+
+    {
+        question: "_____ is principles based while _______ is rule based",
+        answers: {
+            A: "US GAAP, IFRS",
+            B: "IFRS, US GAAP",
+            C: "I dont know, I dont know",
+            D: "none of the above"
+        },
+        correctAnswer: "B",
+        explanation: "IFRS are described as principles based standards and the US GAAP as being rules based."
+    },
+
+    {
+        question: "The Nigerian Accounting Standards Board (NASB) came into being in _________",
+        answers: {
+            A: "1982",
+            B: "1969",
+            C: "1999",
+            D: "2004"
+        },
+        correctAnswer: "A",
+        explanation: "The Nigerian Accounting Standards Board (NASB) came into being in 1982 as an independent body with responsibility for developing and issuing Nigerian accounting Standards."
+    },
+
+    {
+        question: "The following are rules not found in IFRS however companies in industries covered in Nigeria are expected to continue to apply these rules except ______",
+        answers: {
+            A: "SAS 14: Accounting in the petroleum industry: Down-stream activities",
+            B: "SAS 2: Accounting for Environmental and natural conservation",
+            C: "SAS 17: Accounting in the petroleum industry: UP-stream activities",
+            D: "SAS 32: Accounting by not-for-profit organisations"
+        },
+        correctAnswer: "B",
+        explanation: "these relevant standards include SAS 14(Accounting in the petroleum industry: Down-stream activities), SAS 17(Accounting in the petroleum industry: Up-stream activities) SAS 25(Telecommunications activities) SAS 32(ON accounting by not-for-profit organisations)"
+    },
+
+
+];*/
+
+let currentQuestionIndex = 0;
+let score = 0 
+
+function displayQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    document.getElementById('question').innerText = currentQuestion.question;
+    document.getElementsByClassName('answer-btn')[0].innerText = currentQuestion.answers.A;
+    document.getElementsByClassName('answer-btn')[1].innerText = currentQuestion.answers.B;
+    document.getElementsByClassName('answer-btn')[2].innerText = currentQuestion.answers.C;
+    document.getElementsByClassName('answer-btn')[3].innerText = currentQuestion.answers.D;
+    document.getElementById('feedback').innerText = '';
+    document.getElementById('next-btn').style.display = 'none';
+}
+
+
+const feedbackElement = document.getElementById('feedback');
+
+function checkAnswer(selectedAnswer) {
+    const currentQuestion = questions[currentQuestionIndex];
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+        score++
+        feedbackElement.innerText = "Correct!";
+        feedbackElement.style.color = "green";
+    } else {
+        feedbackElement.innerText = `Wrong! ${currentQuestion.explanation}`;
+        feedbackElement.style.color = "red";
+    }
+
+    document.getElementById('next-btn').style.display = 'block';
+}
+
+function nextQuestion() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion();
+    } else {
+        feedbackElement.innerText = " "
+        document.getElementById('question-container').innerHTML = `<h3>You scored ${score} out of ${questions.length} reload to take again</h3>`;
+        document.getElementById('next-btn').style.display = 'none';
+    }
+}
+
+window.onload = displayQuestion;
